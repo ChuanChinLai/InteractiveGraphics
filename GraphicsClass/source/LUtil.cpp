2af4203aebdc356478879f046059ae9566cfa44f
danny6430 <danny6430@hotmail.com>
2018-01-26 12:43:45 -0700

    working on load buffer

--- GraphicsClass/source/LUtil.cpp
+++ GraphicsClass/source/LUtil.cpp
@@ -1,5 +1,15 @@
 #include "LUtil.h"
 
+#include <cyCodeBase\cyTriMesh.h>
+
+#include <vector>
+#include <iostream>
+
+GLuint VAO;
+GLuint VBO;
+
+cy::TriMesh mesh;
+
 bool InitGL()
 {
 	//Initialize GLEW
@@ -10,6 +20,56 @@ bool InitGL()
 		return false;
 	}
 
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	const auto errorCode = glGetError();
+
+	if (errorCode == GL_NO_ERROR)
+	{
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		const auto errorCode = glGetError();
+		if (errorCode != GL_NO_ERROR)
+		{
+			std::cout << "OpenGL failed to bind a new vertex buffer: " << errorCode << std::endl;
+		}
+	}
+	else
+	{
+		std::cout << "OpenGL failed to get an unused vertex buffer ID: " << errorCode << std::endl;
+	}
+
+
+	if (mesh.LoadFromFileObj("teapot.obj"))
+	{
+		std::cout << "Good" << std::endl;
+	}
+	else
+	{
+		std::cout << "Bad" << std::endl;
+	}
+
+	cy::Point3f* Vertices = new cy::Point3f[mesh.NV()];
+
+	for (int i = 0; i < mesh.NV(); i++)
+	{
+		Vertices[i].x = mesh.V(i).x;
+		Vertices[i].y = mesh.V(i).y;
+		Vertices[i].z = mesh.V(i).z;
+	}
+
+	const auto bufferSize = mesh.NV() * sizeof(cy::Point3f);
+
+	glBufferData(GL_ARRAY_BUFFER, bufferSize, Vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	delete[] Vertices;
+
 	return true;
 }
 
@@ -18,9 +78,11 @@ void Update()
 	static float Green = 0.0f;
 	static float delta = 0.01f;
 
-	Green += delta;
+//	Green += delta;
 
-	delta = (Green >= 1.0) ? -0.01f : (Green <= 0.0) ? 0.01f : delta;
+//	delta = (Green >= 1.0) ? -0.01f : (Green <= 0.0) ? 0.01f : delta;
+
+
 
 	glClearColor(0.0f, Green, 0.0f, 0.0f);
 
@@ -47,6 +109,8 @@ void Render()
     //Clear color buffer
     glClear( GL_COLOR_BUFFER_BIT );
 
+	glBindVertexArray(VAO);
+	glDrawArrays(GL_POINTS, 0, mesh.NV());
 
     //Update screen
     glutSwapBuffers();
